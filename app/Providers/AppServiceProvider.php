1c5d634f2e287584e3f4041ab3fa4b53d9f33fb4
Hager Abdo <hager.abdo121@hotmail.com>
2025-05-21 17:54:23 +0300

    feat(auth): add middleware and redirect logic for admin routes
    Add 'guest' middleware to protect login routes and 'auth' middleware to secure dashboard routes. Implement redirect logic in AppServiceProvider to handle authenticated and unauthenticated users, ensuring proper navigation flow within the admin area.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Support\Facades\Session;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //Redirect an Authentacated user to dashboard
+        RedirectIfAuthenticated::redirectUsing(function () {
+            return route('admin.dashboard');
+        });
+
+        //Redirect an unAuthentacated user to admin login page
+        Authenticate::redirectUsing(function () {
+            Session::flash('fail', 'You must be logged in to access admin area. Please login first.');
+            return route('admin.login');
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,14 @@ Route::view('/example-auth', 'example-auth');
  * Admin Routes
  */
 Route::prefix('admin')->name('admin.')->group(function () {
-    Route::middleware([])->group(function () {
+    Route::middleware(['guest'])->group(function () {
         Route::controller(AuthController::class)->group(function () {
             Route::get('/login', 'loginForm')->name('login');
             Route::post('/login', 'loginHandler')->name('login_handler');
             Route::get('/forgot-password', 'forgotForm')->name('forgot');
         });
     });
-    Route::middleware([])->group(function () {
+    Route::middleware(['auth'])->group(function () {
         Route::controller(AdminController::class)->group(function () {
             Route::get('/dashboard', 'adminDashboard')->name('dashboard');
             Route::post('/logout', 'logoutHandler')->name('logout');
