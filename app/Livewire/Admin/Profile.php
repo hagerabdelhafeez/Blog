a9f91edc1c1f704bf437f4b0391a6ae362553a24
Hager Abdo <hager.abdo121@hotmail.com>
2025-05-27 01:35:34 +0300

    feat(profile): add password update functionality with email notification
    Implement password update feature in admin profile section including:
    - Current password validation
    - New password confirmation
    - Email notification upon successful change
    - Automatic logout after password change
    - Error handling for failed updates

--- app/Livewire/Admin/Profile.php
+++ app/Livewire/Admin/Profile.php
@@ -6,13 +6,17 @@ use Livewire\Component;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use App\Helpers\CMail;
 
 class Profile extends Component
 {
     public $tab = null;
     public $tabName = 'personal_details';
-    public $name, $email, $username, $bio;
     protected $queryString = ['tab' => ['keep' => true]];
+    public $name, $email, $username, $bio;
+    public $current_password, $new_password, $new_password_confirmation;
 
     protected $listeners = [
         'updateProfile' => '$refresh'
@@ -54,6 +58,49 @@ class Profile extends Component
                 'draggable' => true
             ]);
             $this->dispatch('updateTopUserInfo')->to(TopUserInfo::class);
+        } else {
+            $this->dispatch('swalAlert', [
+                'title' => "Oops...\n Something went wrong!",
+                'icon' => 'error',
+                'draggable' => true
+            ]);
+        }
+    }
+
+    public function updatePassword()
+    {
+        $user = User::findOrFail(Auth::user()->id);
+        $this->validate([
+            'current_password' => [
+                'required',
+                'min:5',
+                function ($attribute, $value, $fail) use ($user) {
+                    if (!Hash::check($value, $user->password)) {
+                        return $fail('Your current password does not match our records.');
+                    }
+                }
+            ],
+            'new_password' => 'required|min:5|confirmed',
+        ]);
+        $updated = $user->update([
+            'password' => Hash::make($this->new_password)
+        ]);
+        if ($updated) {
+            $data = array(
+                'user' => $user,
+                'new_password' => $this->new_password,
+            );
+            $mail_body = view('email-templates.password-changes-template', $data)->render();
+            $mail_config = array(
+                'recipient_address' => $user->email,
+                'recipient_name' => $user->name,
+                'subject' => 'Password Changes',
+                'body' => $mail_body,
+            );
+            CMail::send($mail_config);
+            Auth::logout();
+            Session::flash('info', 'Your password has been changed successfully. Please login with your new password.');
+            $this->redirectRoute('admin.login');
         } else {
             $this->dispatch('swalAlert', [
                 'title' => "Oops...\n Something went wrong!",
--- resources/views/livewire/admin/profile.blade.php
+++ resources/views/livewire/admin/profile.blade.php
@@ -3,9 +3,12 @@
         <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 mb-30">
             <div class="pd-20 card-box height-100-p">
                 <div class="profile-photo">
-                    <a href="javascript:;" onclick="event.preventDefault();document.getElementById('profilePictureFile').click();" class="edit-avatar"><i class="fa fa-pencil"></i></a>
+                    <a href="javascript:;"
+                        onclick="event.preventDefault();document.getElementById('profilePictureFile').click();"
+                        class="edit-avatar"><i class="fa fa-pencil"></i></a>
                     <img src="{{ $user->picture }}" alt="" class="avatar-photo" id="profilePicturePreview">
-                    <input type="file" name="profilePictureFile" id="profilePictureFile" class="d-none" style="opacity: 0">
+                    <input type="file" name="profilePictureFile" id="profilePictureFile" class="d-none"
+                        style="opacity: 0">
                 </div>
                 <h5 class="text-center h5 mb-0">{{ $user->name }}</h5>
                 <p class="text-center text-muted font-14">
@@ -74,28 +77,35 @@
                     <div class="tab height-100-p">
                         <ul class="nav nav-tabs customtab" role="tablist">
                             <li class="nav-item">
-                                <a wire:click="selectTab('personal_details')" class="nav-link {{ $tab == 'personal_details' ? 'active' : '' }}"  data-toggle="tab" href="#personal_details"
-                                    role="tab">Personal details</a>
+                                <a wire:click="selectTab('personal_details')"
+                                    class="nav-link {{ $tab == 'personal_details' ? 'active' : '' }}" data-toggle="tab"
+                                    href="#personal_details" role="tab">Personal details</a>
                             </li>
                             <li class="nav-item">
-                                <a wire:click="selectTab('update_password')" class="nav-link {{ $tab == 'update_password' ? 'active' : '' }}" data-toggle="tab" href="#update_password" role="tab">Update
+                                <a wire:click="selectTab('update_password')"
+                                    class="nav-link {{ $tab == 'update_password' ? 'active' : '' }}" data-toggle="tab"
+                                    href="#update_password" role="tab">Update
                                     password</a>
                             </li>
                             <li class="nav-item">
-                                <a  wire:click="selectTab('social_links')" class="nav-link {{ $tab == 'social_links' ? 'active' : '' }}" data-toggle="tab" href="#social_links" role="tab">Social
+                                <a wire:click="selectTab('social_links')"
+                                    class="nav-link {{ $tab == 'social_links' ? 'active' : '' }}" data-toggle="tab"
+                                    href="#social_links" role="tab">Social
                                     links</a>
                             </li>
                         </ul>
                         <div class="tab-content">
                             <!-- personal details Tab start -->
-                            <div class="tab-pane fade {{ $tab == 'personal_details' ? 'show active' : '' }}" id="personal_details" role="tabpanel">
+                            <div class="tab-pane fade {{ $tab == 'personal_details' ? 'show active' : '' }}"
+                                id="personal_details" role="tabpanel">
                                 <div class="pd-20">
                                     <form wire:submit='updatePersonalDetails()'>
                                         <div class="row">
                                             <div class="col-md-12">
                                                 <div class="form-group">
                                                     <label>Full Name</label>
-                                                    <input type="text" class="form-control" wire:model='name' placeholder="Enter full name">
+                                                    <input type="text" class="form-control" wire:model='name'
+                                                        placeholder="Enter full name">
                                                     @error('name')
                                                         <span class="text-danger">{{ $message }}</span>
                                                     @enderror
@@ -104,7 +114,8 @@
                                             <div class="col-md-6">
                                                 <div class="form-group">
                                                     <label>Email</label>
-                                                    <input type="text" class="form-control" wire:model='email' placeholder="Enter email address" disabled>
+                                                    <input type="text" class="form-control" wire:model='email'
+                                                        placeholder="Enter email address" disabled>
                                                     @error('email')
                                                         <span class="text-danger">{{ $message }}</span>
                                                     @enderror
@@ -113,7 +124,8 @@
                                             <div class="col-md-6">
                                                 <div class="form-group">
                                                     <label>Username</label>
-                                                    <input type="text" class="form-control" wire:model='username' placeholder="Enter username">
+                                                    <input type="text" class="form-control" wire:model='username'
+                                                        placeholder="Enter username">
                                                     @error('username')
                                                         <span class="text-danger">{{ $message }}</span>
                                                     @enderror
@@ -137,14 +149,51 @@
                             </div>
                             <!-- personal details Tab End -->
                             <!-- update password Tab start -->
-                            <div class="tab-pane fade {{ $tab == 'update_password' ? 'show active' : '' }}" id="update_password" role="tabpanel">
+                            <div class="tab-pane fade {{ $tab == 'update_password' ? 'show active' : '' }}"
+                                id="update_password" role="tabpanel">
                                 <div class="pd-20 profile-task-wrap">
-                                    ---- update password ----
+                                    <form wire:submit='updatePassword()'>
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <label for="">Current password</label>
+                                                    <input type="password" class="form-control"
+                                                        wire:model='current_password'
+                                                        placeholder="Enter current password">
+                                                    @error('current_password')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <label for="">New password</label>
+                                                    <input type="password" class="form-control"
+                                                        wire:model='new_password' placeholder="Enter new password">
+                                                    @error('new_password')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <label for="">Confirm new password</label>
+                                                    <input type="password" class="form-control"
+                                                        wire:model='new_password_confirmation' placeholder="Confirm new password">
+                                                    @error('new_password_confirmation')
+                                                        <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <button type="submit" class=" btn btn-primary">Update password</button>
+                                    </form>
                                 </div>
                             </div>
                             <!-- update password Tab End -->
                             <!-- Setting Tab start -->
-                            <div class="tab-pane fade {{ $tab == 'social_links' ? 'show active' : '' }}" id="social_links" role="tabpanel">
+                            <div class="tab-pane fade {{ $tab == 'social_links' ? 'show active' : '' }}"
+                                id="social_links" role="tabpanel">
                                 <div class="pd-20 profile-task-wrap">
                                     ---- social links ----
                                 </div>
